017f8f266e29bdb7f134a7b14677a1494a68bc06
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-10-23 16:24:10 +0300

    add file formatting

--- Sources/StackNavigator/Builder/AppBuilder.swift
+++ Sources/StackNavigator/Builder/AppBuilder.swift
@@ -1,23 +1,19 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by mehmet karanlık on 23.10.2022.
 //
 
 import SwiftUI
 
-
-
-
-
-struct AppBuilder: View  {
-   let routes : [PageRouteInfo]
-   let initRoute : PageRouteInfo?
+struct AppBuilder: View {
+   let routes: [PageRouteInfo]
+   let initRoute: PageRouteInfo?
 
    init(routes: [PageRouteInfo]) {
       self.routes = routes
-      initRoute = routes.first(where: {$0.isInitial})
+      initRoute = routes.first(where: { $0.isInitial })
       assert(initRoute != nil, "Initial route has not been provided.")
    }
 
